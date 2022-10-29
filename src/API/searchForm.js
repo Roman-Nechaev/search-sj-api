f683798ce1d8d783cefe00d52b70eb45ec9a1be3
Roman-Nechaev <nrchaevroman@gmail.com>
2022-10-30 01:59:00 +0300

    c

--- src/API/searchForm.js
+++
@@ -1,9 +0,0 @@
-import { getUser } from './API/server-request';
-
-const searchFormRef = document.querySelector('.search-form');
-searchFormRef.addEventListener('click', () => {});
-const searchFormSubmit = evt => {
-  evt.preventDefault();
-  const { currentTarget } = evt;
-  console.log(currentTarget);
-};
